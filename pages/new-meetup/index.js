18f888fe022ed7caf1063b74f1e190f5b1fbd0dd
nimishak02 <nimishacr02@gmail.com>
2023-06-14 19:19:05 +0530

    new commit

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,39 @@
+//our-domain.com/new-meetup
+import { Fragment } from "react";
+import Head from "next/head";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
+
+function NewMeetupPage() {
+  const router = useRouter();
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push("/");
+  }
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a new Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetup and create amazing networking  oportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
+}
+
+export default NewMeetupPage;
